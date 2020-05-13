0a960409b505391ceeaa7b81d4b30d8657b77deb
Zeb Pykosz <zeb.apy@gmail.com>
2020-05-13 16:36:52 -0400

    add sorted and grouped names page

--- src/_data/seniors.js
+++ src/_data/seniors.js
@@ -1,15 +1,31 @@
 const path = require('path');
 const fs = require('fs');
 const Papa = require('papaparse');
+const { groupBy, sortBy } = require('lodash');
 
 module.exports = () => {
   const csv = fs.readFileSync(path.resolve(__dirname, 'seniors.csv'), {
     encoding: 'utf8'
   });
 
-  const data = Papa.parse(csv, {
+  const { data } = Papa.parse(csv, {
     header: true
   });
 
-  return data.data;
+  const grouped = groupBy(data, (person) =>
+    person.last_name.charAt(0).toLowerCase()
+  );
+
+  const sorted = Object.keys(grouped)
+    .sort()
+    .map((letter) => {
+      return {
+        letter,
+        names: sortBy(grouped[letter], (person) =>
+          person.last_name.toLowerCase()
+        )
+      };
+    });
+
+  return sorted;
 };
