8f1a135e30727908df5d129a94a0f077e7c876f4
zebapy <zebapy@users.noreply.github.com>
2020-05-24 16:44:06 +0000

    Automated deployment: Sun May 24 16:44:06 UTC 2020 caa5f870f36a3d889542735e4e33f83eaf902497

---
+++ js/main.js
@@ -0,0 +1,51 @@
+(function () {
+  var video = document.querySelector('[data-video]');
+
+  function showVideo() {
+    var content = video.querySelector('[data-video-content]');
+    var btn = video.querySelector('[data-video-btn]');
+    var url = btn.href;
+
+    var iframe = document.createElement('iframe');
+    iframe.src = url + '?autoplay=1';
+    iframe.allow = 'autoplay; encrypted-media';
+    iframe.setAttribute('allowfullscreen', 'allowfullscreen');
+    // ie11 doesn't support multi arg classList.add()
+    iframe.setAttribute('class', 'absolute inset-0 h-full w-full');
+
+    content.innerHTML = '';
+
+    content.appendChild(iframe);
+  }
+
+  if (video) {
+    video.onclick = function (event) {
+      event.preventDefault();
+      showVideo();
+    };
+  }
+
+  var transcriptBtn = document.querySelector('.js-transcript-btn');
+  var transcriptBody = document.querySelector('.js-transcript-body');
+  var transcriptBtnIcon = document.querySelector('.js-transcript-icon');
+
+  if (transcriptBtn) {
+    transcriptBtn.onclick = function () {
+      transcriptBody.classList.toggle('hidden');
+      transcriptBtnIcon.classList.toggle('-rotate-180');
+    };
+  }
+
+  // mobile toggler
+
+  var menuBtn = document.querySelector('.js-menu-btn');
+  var menu = document.querySelector('.js-menu');
+  var timesIcon = menuBtn.querySelector('.times');
+  var barsIcon = menuBtn.querySelector('.bars');
+
+  menuBtn.onclick = function () {
+    menu.classList.toggle('hidden');
+    barsIcon.classList.toggle('hidden');
+    timesIcon.classList.toggle('hidden');
+  };
+})();
